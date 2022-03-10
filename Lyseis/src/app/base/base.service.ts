c1a41e369323d04a4fc83f328bdba4dd7f87b5c4
Juan Manuel De Lós Ríos <attiscolombia@gmail.com>
2022-03-10 17:31:23 -0500

    improve the base service

--- Lyseis/src/app/base/base.service.ts
+++ Lyseis/src/app/base/base.service.ts
@@ -1,38 +1,69 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {environment} from '../../environments/environment';
+import { environment } from '../../environments/environment';
 import Utils from '../libs/utils';
+import { catchError } from 'rxjs/operators';
+
 export default class BaseService {
 
     constructor(public http: HttpClient) {
 
     }
-    
-    public ConsumeService(endpoint: string, verb: 'POST'|'GET'|'PUT'|'DELETE', body: unknown, contentType: 'application/json'|'application/x-www-form-urlencoded' = 'application/json', headers?: HttpHeaders): Observable<any> {
+
+    public ConsumeService(endpoint: string, verb: 'POST' | 'GET' | 'PUT' | 'DELETE', body?: unknown, contentType: 'application/json' | 'application/x-www-form-urlencoded' = 'application/json', headers?: HttpHeaders): Observable<any> {
         return new Observable(observer => {
-            if(!headers){
-                headers = this.SetDefaultHeaders();
+
+            let defaultHeaders = this.SetDefaultHeaders();
+
+            if (!headers) {
+                headers = defaultHeaders;
+            } else {
+
+                // concat all headers
+                let headerValue = '';
+                headers.keys().forEach(headerKey => {
+                    headerValue = headers?.get(headerKey) as string;
+                    defaultHeaders[headerKey] = headerValue;
+                });
             }
 
-            headers?.append('Content-Type', contentType);
+            defaultHeaders['Content-Type'] = contentType;
+
             switch (verb) {
                 case 'POST':
-                    this.http.post(`${environment.baseUrl}/${endpoint}`, body, {headers: headers}).subscribe(response => {
-                        observer.next(response)
+                    this.http.post(`${environment.baseUrl}/${endpoint}`, body, { headers: defaultHeaders })
+                    .subscribe({
+                        next: response => {
+                            observer.next(response);
+                        },
+                        error: error => {
+                            observer.error(error);
+                        }
                     })
                     break;
 
                 case 'GET':
-                    this.http.get(`${environment.baseUrl}/${endpoint}`).subscribe(response => {
-                        observer.next(response);
-                    })
+                    let params: HttpParams = body as HttpParams;
+                    this.http.get(`${environment.baseUrl}${endpoint}`, { headers: defaultHeaders, params: params })
+                        .subscribe({
+                            next: response => {
+                                observer.next(response);
+                            },
+                            error: error => {
+                                observer.error(error)
+                            }
+                        });
+
                     break;
             }
         });
     }
-    SetDefaultHeaders(): HttpHeaders {
-        let headers = new HttpHeaders();
-        headers.append('Authorization', `bearer ${Utils.GetSessionStorage('token')}`);
+    SetDefaultHeaders(): any {
+        let headers: any = {};
+        let token = Utils.GetSessionStorage('token');
+        if (token != '' || token == null) {
+            headers['Authorization'] = `bearer ${token}`;
+        }
 
         return headers;
     }
